e205fee8dedf003d96391b1f648e4793bcd45f7d
rainbowZHOU529 <240871498@qq.com>
2020-07-06 12:32:04 +0800

    add banners

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,31 +1,42 @@
 <template>
   <div>
     <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
     <h2>首页</h2>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar';
-  // import axios from 'axios'
-  import {request} from 'network/request';
+  import HomeSwiper from './childComps/HomeSwiper'
+
+  // import {request} from 'network/request';
   import {getHomeMultidata} from 'network/home';
 
   export default {
     name:'Home',
     components:{
-      NavBar
+      NavBar,HomeSwiper
     },
     created(){
-      request({url:'/home/multidata'}).then( res=>{
-        // console.log('request:'+res);
-        console.log(res);
-      });
+      // request({url:'/home/multidata'}).then( res=>{
+      //   console.log(res);
+      // });测试request请求
+      
       getHomeMultidata().then( res => {
-        // console.log('getHomeMultidata:'+res);
+        this.banners = res.data.data.banner.list;
+        console.log(this.banners);
+        
+        this.recommends = res.data.data.recommend.list;
         console.log(res);
       })
-    }
+    },
+    data() {
+      return {
+        banners:[],
+        recommends:[]
+      }
+    },
   }
 </script>
 
